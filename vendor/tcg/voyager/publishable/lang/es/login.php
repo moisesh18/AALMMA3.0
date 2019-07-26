5706d7c49bbfcedff013b119066437e1c4111dc9
Moises Hernandez <moisesher18@gmail.co>
2019-07-25 22:57:28 -0500

    Revert "Revert "vendor""
    This reverts commit 234cd18641dbc6cb1a5714bb18760812382b82ef.

---
+++ vendor/tcg/voyager/publishable/lang/es/login.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'loggingin'    => 'Iniciando sesión',
+    'signin_below' => 'Iniciar sesión',
+    'remember_me' => 'Recordarme',
+    'welcome'      => 'Bienvenido a Voyager. El administrador desaparecido de Laravel ',
+];
